5d007fd44a56290adcbe5a8f1d4e0fb933e7358e
suttp <suttp@LAPTOP-NONC3BGN>
2026-03-18 13:14:40 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace BCSH1_SemestralniPrace_Suttner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
